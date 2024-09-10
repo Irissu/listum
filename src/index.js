704092ef4893b33bcdb64b3b9594fa1d0d64d361
Iris <irismo91@gmail.com>
2024-09-10 10:33:44 +0200

    CSS of all elements done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 console.log("Hellooo");
 const modalWindow = document.getElementById("modal-window");
 const cancel = document.getElementById("cancel");
+const submit = document.getElementById("submit");
 
 document.addEventListener("click", function (event) {
   if (event.target.classList.contains("open-modal")) {
@@ -16,4 +17,8 @@ window.addEventListener("click", function (event) {
   if (event.target === modalWindow) {
     modalWindow.style.display = "none";
   }
+});
+
+submit.addEventListener("click", function () {
+  console.log("Hi!!");
 });
