4bc2ef3539157589443ee903695ecd5090519855
Iris <irismo91@gmail.com>
2024-09-20 20:19:03 +0200

    function to create notes and display them OK

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-console.log("Hellooo");
 const modalWindow = document.getElementById("modal-window");
 const cancel = document.getElementById("cancel");
 const submit = document.getElementById("submit");
 const wrapper = document.querySelector(".wrapper");
+const addNewNote = document.querySelector(".postit.new-note");
 const titleModal = modalWindow.querySelector("input");
 const textModal = modalWindow.querySelector("textarea");
 
@@ -13,6 +13,8 @@ document.addEventListener("click", function (event) {
 });
 
 cancel.addEventListener("click", function () {
+  titleModal.value = "";
+  textModal.value = "";
   modalWindow.classList.remove("modal");
 });
 
@@ -31,21 +33,40 @@ wrapper.addEventListener("click", function (event) {
 
 document.querySelectorAll(".delete-note").forEach(element => element.addEventListener(
   "click", () => {
-    console.log("hi, Im a delete button");
+    console.log("hi, I'm a delete button");
   }
 ));
 
+function loadNotes() {
+  document.querySelectorAll(".note").forEach(element => element.remove());
+  const notes = getNotesLS();
+  notes.forEach((note) => {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("postit", "note");
+    newDiv.innerHTML = `
+    <h4 class="postit-title">${note.title}</h4>
+        <p class="postit-text">${note.description}</p>
+        <div class="info-note">
+          <small class="postit-date">${note.date}</small>
+          <div>
+            <button class="edit-note"><i class="fa-solid fa-pencil"></i></button>
+            <button class="delete-note"><i class="fa-solid fa-trash-can"></i></button>
+          </div>
+        </div>`;
+    addNewNote.insertAdjacentElement("afterend", newDiv);
+  });
+}
+loadNotes();
+
 submit.addEventListener("click", function (event) {
-  console.log("Hi!!");
+  console.log("Hi!, you clicked on the submit button");
   event.preventDefault();
   const noteTitle = titleModal.value;
   const noteText = textModal.value;
   const dateObj = new Date();
   const dateFormated = dateObj.toLocaleDateString();
 
-  // esto podria ser una funcion aparte que se le de como parametro noteTitle y noteText, crea el objeto  y lo devuelve
   if (noteTitle && noteText) {
-    // aqui borro mensaje de error si existe
     console.log(`${noteTitle} ${noteText} ${dateFormated}`);
     const note = {
       title: noteTitle,
@@ -53,46 +74,25 @@ submit.addEventListener("click", function (event) {
       date: dateFormated
     };
 
-    const notes = getNotesLS(); // recojo todas las notas que hay en LS, esto es un array
-    notes.push(note); // añado la nuevan ota en el array (recuerda, esto no es el LS)
-    // aqui deberia ir una funcion que recoge un array de notas y crea en HTML con ellas????
-    saveNote(notes); // guardo las notas en el LS (el array)
-    cancel.click(); // igual que si hubiese hecho click en cancel
+    const notes = getNotesLS();
+    notes.push(note);
+    saveNote(notes);
+    loadNotes();
+    cancel.click();
   } else {
     console.log("Debes rellenar los campos de titulo y mensaje.");
-    // aqui quiero meter un mensaje de error
   }
 });
 
-/* FUNCIONES QUE TENGO QUE HACER
-1 - saveNote() FUNCION QUE GUARDE UNA NOTA EN LOCAL STORAGE
-
-2 - showNotes() FUNCION QUE MUESTRE TODAS LAS NOTAS GUARDADAS. SE EJECUTA AL PRINCIPIO DEL SCRIPT SIEMPRE
-
-3- deleteNote() FUNCION QUE BORRA UNA NOTA DE LOCAL STORAGE, LUEGO  MUESTRA DE NUEVO LAS NOTAS
-
-4- editNote() FUNCION QUE EDITA UNA NOTA DE LOCAL STORAGE, LUEGO  MUESTRA DE NUEVO LAS NOTAS
-
-MODAL NEW NOTE: SUBMIT BUTTON HACE: COMPRUEBA QUE LA NOTA NO ESTE VACÍA. SI NO ESTÁ VACIA LA CREA  saveNote() y luego showNotes()
-MODAL EDIT NOTE: EDIT BUTTON HACE:
-*/
-
 getNotesLS().forEach((note) => {
-  /* const noteElement = createNoteDOM(note.id, note.title, note.content); */
   console.log("I'm a forEach");
   console.log(note);
 });
 
 function getNotesLS() {
-  // returns the array of notes saved in LogalStorage
   return JSON.parse(localStorage.getItem("notesParsed") || "[]");
 }
 
 function saveNote(note) {
-  // guardamos la nota en local storage, es un objeto notes, pero primero lo pasamos a JSON
   localStorage.setItem("notesParsed", JSON.stringify(note));
-}
-
-function createNoteDOM(id, title, content) {
-  notes = getNotesLS();
 }
